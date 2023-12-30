270a5a57baba157b096cce1a040bd38874bdbe1b
Chin Su <viet129@hotmail.com>
2023-12-30 16:32:54 +0700

    Add function player attack

--- Assets/Scripts/Projectle/BulletController.cs
+++ Assets/Scripts/Projectle/BulletController.cs
@@ -10,29 +10,36 @@ public class BulletController : MonoBehaviour
     private float timeLife;
     private int direction = 1;
     private new Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
     private Animator animator;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
-        rigidbody.velocity = Vector2.right * speed * direction;
+        if (rigidbody != null)
+            rigidbody.velocity = Vector2.right * speed * direction;
         timeLife -= Time.deltaTime;
         if (timeLife < 0)
             gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// Used to set direction move for bullet
+    /// </summary>
+    /// <param name="direction">Enter int value, 1 to move left-right, -1 to move right-left</param>
     public void SetDirection(int direction)
     {
         timeLife = 3;
         this.direction = direction;
         var localScale = transform.localScale;
         if (direction < 0)
-            localScale = new Vector3(-localScale.x, localScale.y, localScale.z);
+            spriteRenderer.flipX = false;
     }
 
     public void Destroy()
@@ -42,7 +49,7 @@ public class BulletController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SetDirection(0);
+        direction = 0;
         animator.SetTrigger(explore);
     }
 }
