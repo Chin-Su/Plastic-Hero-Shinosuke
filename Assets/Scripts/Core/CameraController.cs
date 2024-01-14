6561573327ba61ce1b9d7e14e5a5d168ff120540
Chin Su <viet129@hotmail.com>
2024-01-14 14:55:34 +0700

    Add function follow player for camera

---
+++ Assets/Scripts/Core/CameraController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Properties")]
+    [SerializeField] private float minPosX;
+
+    [SerializeField] private float maxPosX;
+    [SerializeField] private float minPosY;
+    [SerializeField] private float maxPosY;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+
+    [Header("Target")]
+    [SerializeField] private Transform player;
+
+    private float currentPosX;
+    private float lookAhead;
+
+    private void Update()
+    {
+        // Calculate pos x and pos y of camera
+        float xPos = Mathf.Clamp(player.position.x + lookAhead, minPosX, maxPosX);
+        float yPos = Mathf.Clamp(player.position.y + 2, minPosY, maxPosY);
+
+        // Move camera
+        transform.position = new Vector3(xPos, yPos, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * Mathf.Sign(player.localScale.x)), Time.deltaTime * cameraSpeed);
+    }
+}
