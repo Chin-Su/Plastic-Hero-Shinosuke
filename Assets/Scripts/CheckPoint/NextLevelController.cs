dfb48bc43a7154b9bf0ca2bbf2055d791fefb561
Chin Su <viet129@hotmail.com>
2024-01-15 23:02:47 +0700

    Add checkpoint next level and handle logic for it

---
+++ Assets/Scripts/CheckPoint/NextLevelController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelController : MonoBehaviour
+{
+    [SerializeField] private string nextLevel;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            this.PostEvent(EventId.Winner);
+            Timer.Instance.Schedule(() => { SceneManager.LoadScene(nextLevel); }, 3.0f);
+        }
+    }
+}
