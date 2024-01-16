50fd46a8177da6f7e1bf538dd4b23ca0559166a2
Chin Su <viet129@hotmail.com>
2024-01-16 22:35:18 +0700

    Fix clean code

--- Assets/Scripts/Core/Timer.cs
+++ Assets/Scripts/Core/Timer.cs
@@ -2,42 +2,23 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public class Timer : MonoBehaviour
+public class Timer
 {
-    private static Timer instance;
-
-    public static Timer Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                GameObject singleton = new GameObject("Timer-Singleton");
-                singleton.AddComponent<Timer>();
-            }
-            return instance;
-        }
-        private set { }
-    }
-
-    private void Awake()
-    {
-        if (instance != null && instance.GetInstanceID() != GetInstanceID())
-            Destroy(gameObject);
-        instance = this;
-    }
+    private static MonoBehaviour timer;
 
     /// <summary>
     /// Used to handle action after time delay
     /// </summary>
     /// <param name="action">Action need to execute</param>
     /// <param name="timeDelay">Time delay</param>
-    public void Schedule(Action action, float timeDelay)
+    public static void Schedule(MonoBehaviour monoBehaviour, Action action, float timeDelay)
     {
-        StartCoroutine(DelayAction(action, timeDelay));
+        timer = monoBehaviour;
+        if (timer != null)
+            timer.StartCoroutine(DelayAction(action, timeDelay));
     }
 
-    private IEnumerator DelayAction(Action action, float delay)
+    private static IEnumerator DelayAction(Action action, float delay)
     {
         yield return new WaitForSeconds(delay);
         action?.Invoke();
