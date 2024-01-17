e54b587afc5d1f0d3e53dff568bed4ad19ff0e4c
Chin Su <viet129@hotmail.com>
2024-01-17 22:55:54 +0700

    Add UI canvas and demo design pause dialog

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Continue()
+    {
+        Debug.Log("Continue");
+    }
+
+    public void Restart()
+    {
+        Debug.Log("Restart");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
